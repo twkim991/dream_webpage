d2f30184f8c87866b70a7cbd414cd927c8aa7f00
twkim991 <darkbell991@gmail.com>
2023-11-10 01:44:48 +0900

    오름차순내림차순, 최신순, 정확도순, 가격순 버튼 추가 및 검색과 배열 함수 분리

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('search/', views.search, name='search')  # 데이터를 처리할 url 등록!
+    path('search/', views.search, name='search'),  # 데이터를 처리할 url 등록!
+    path('search/ordered', views.reorder, name='reorder')
 ]
--- marketplace/views.py
+++ marketplace/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.db import connection
 from Searcher import Searcher
 from .forms import KeywordForm
+from django.core.cache import cache
 
 def index(request):
     joonggo = []
@@ -31,79 +32,14 @@ def index(request):
     return render(request, 'index.html', {'joonggo_list': joonggo})
 
 def search(request):
+    print('search 페이지로 가는 중')
     joonggo = []
-    if request.method == 'POST':
-        # print(request.POST)
-        form = KeywordForm(request.POST)
-        # print(form)
-        # 유효성 검사
-        print(form.is_valid())
-        if form.is_valid():
-            keyword = form.cleaned_data['keyword']
-            print(keyword)
-            # db에서 중고거래 데이터를 가져온다
-            try:
-                db = connection.cursor()
-                id_list = Searcher.Search(connection,keyword) 
-                print(id_list[0].url)
-                qry = f"""SELECT url, platform, issoldout, title, price, text, isad FROM dream_joonggo.joonggo_data WHERE url IN ({','.join("'" + str(i.url) + "'" for i in id_list)}) ORDER BY id LIMIT 1000;"""
-                # print(qry)
-                db.execute(qry)
-                data = db.fetchall()
-                print(data[0][0])
-                connection.close()
+    if request.method == 'GET':
+        # print(request.GET)
 
-                # 여기에 이미지 정보도 같이 가져와야한다.
-                db2 = connection.cursor()
-                qry2 = "SELECT url, img_url FROM dream_joonggo.joonggo_img WHERE url IN (%s)"
-                url_list = [res[0] for res in data]
-                placeholders = ', '.join(['%s'] * len(url_list))
-                qry2 = qry2 % placeholders
-                db2.execute(qry2, url_list)
-                imgdata = db2.fetchall()
-
-                # 이미지 데이터를 그룹화하기 위한 딕셔너리 생성
-                imgdata_dict = {}
-                try:
-                    for img in imgdata:
-                        url = img[0]
-                        imgurl = img[1]
-                        if url not in imgdata_dict:
-                            imgdata_dict[url] = []
-                        imgdata_dict[url].append(imgurl)
-                except Exception as err:
-                    print(err)
-
-                # print(imgdata_dict)
-
-                for dict2 in data:
-                    for dict1 in id_list:
-                        if dict1.url == dict2[0]:
-                            merged_dict = {
-                                'url': dict2[0],
-                                'platform': dict2[1],
-                                'title' : dict2[3],
-                                'price' : dict2[4],
-                                'text' : dict2[5],
-                                'isad' : dict2[6],
-                                'issoldout' : dict2[2],
-                                'score' : dict1.score,
-                                'imgurl': imgdata_dict.get(dict2[0],[])
-                            }
-                            joonggo.append(merged_dict)
-
-                print(joonggo[0])
-
-            except Exception as err:
-                connection.rollback()
-                print("에러 발생")
-                print(err)
-            
-        # print(joonggo[0])
-        return render(request, 'search.html', {'joonggo_list': joonggo})
-    elif request.method == 'GET':
-        cat = request.GET.get('category')
-        print(cat)
+        cat = request.GET.get('category', 'all')
+        keyword = request.GET.get('keyword', '*')
+        id_list = list()
         category = ''
         if cat == "001":
             category = '데스크탑/본체'
@@ -123,13 +59,146 @@ def search(request):
             category = '소모품'
         else:
             category = 'all'
+        print(f'\n{category}\n{keyword}\n')
         # db에서 중고거래 데이터를 가져온다
         try:
             db = connection.cursor()
+            qry = ''
             if category == 'all':
-                qry = f"SELECT url, platform, issoldout, title, price, text, isad FROM dream_joonggo.joonggo_data ORDER BY id LIMIT 1000;"
+                if keyword == '*':
+                    qry = f"SELECT url, platform, issoldout, title, price, text, isad, id FROM dream_joonggo.joonggo_data ORDER BY id LIMIT 1000;"
+                else:
+                    id_list = Searcher.Search(connection,keyword) 
+                    print(id_list[0].url)
+                    qry = f"""SELECT url, platform, issoldout, title, price, text, isad, id FROM dream_joonggo.joonggo_data WHERE url IN ({','.join("'" + str(i.url) + "'" for i in id_list)}) ORDER BY id LIMIT 1000;"""
             else:
-                qry = f"SELECT url, platform, issoldout, title, price, text, isad FROM dream_joonggo.joonggo_data WHERE maincategory = '{category}' OR subcategory = '{category}' ORDER BY id LIMIT 1000;"
+                if keyword == '*':
+                    qry = f"SELECT url, platform, issoldout, title, price, text, isad, id FROM dream_joonggo.joonggo_data WHERE maincategory = '{category}' OR subcategory = '{category}' ORDER BY id LIMIT 1000;"
+                else:
+                    id_list = Searcher.Search(connection,keyword) 
+                    print(id_list[0].url)
+                    qry = f"""SELECT url, platform, issoldout, title, price, text, isad, id FROM dream_joonggo.joonggo_data WHERE url IN ({','.join("'" + str(i.url) + "'" for i in id_list)}) AND (maincategory = '{category}' OR subcategory = '{category}') ORDER BY id LIMIT 1000;"""
+            db.execute(qry)
+            data = db.fetchall()
+            # print(data[0])
+            connection.close()
+
+            # 여기에 이미지 정보도 같이 가져와야한다.
+            db2 = connection.cursor()
+            qry2 = "SELECT url, img_url FROM dream_joonggo.joonggo_img WHERE url IN (%s)"
+            url_list = [res[0] for res in data]
+            placeholders = ', '.join(['%s'] * len(url_list))
+            qry2 = qry2 % placeholders
+            db2.execute(qry2, url_list)
+            imgdata = db2.fetchall()
+
+            # 이미지 데이터를 그룹화하기 위한 딕셔너리 생성
+            imgdata_dict = {}
+            try:
+                for img in imgdata:
+                    url = img[0]
+                    imgurl = img[1]
+                    if url not in imgdata_dict:
+                        imgdata_dict[url] = []
+                    imgdata_dict[url].append(imgurl)
+            except Exception as err:
+                print(err)
+
+            # print(imgdata_dict)
+
+            if keyword != '*':
+                for dict2 in data:
+                    for dict1 in id_list:
+                        if dict1.url == dict2[0]:
+                            merged_dict = {
+                                'url': dict2[0],
+                                'platform': dict2[1],
+                                'title' : dict2[3],
+                                'price' : dict2[4],
+                                'text' : dict2[5],
+                                'isad' : dict2[6],
+                                'issoldout' : dict2[2],
+                                'score' : dict1.score,
+                                'id' : dict2[7],
+                                'imgurl': imgdata_dict.get(dict2[0],[])
+                            }
+                            joonggo.append(merged_dict)
+            else:
+                for dict2 in data:
+                    merged_dict = {
+                        'url': dict2[0],
+                        'platform': dict2[1],
+                        'title' : dict2[3],
+                        'price' : dict2[4],
+                        'text' : dict2[5],
+                        'isad' : dict2[6],
+                        'issoldout' : dict2[2],
+                        'score' : 0,
+                        'id' : dict2[7],
+                        'imgurl': imgdata_dict.get(dict2[0],[])
+                    }
+                    joonggo.append(merged_dict)
+
+        except Exception as err:
+            connection.rollback()
+            print("에러 발생")
+            print(err)
+        
+        # print(joonggo[0])
+        # 데이터를 캐시에 저장
+        cache.set('joonggo_list', joonggo, timeout=3600)  # 1시간 동안 유효
+        return render(request, 'search.html', {'joonggo_list': joonggo})
+    
+def reorder(request):
+    print('search/ordered 페이지로 가는 중')
+    joonggo = []
+    # 캐시에서 중고거래 데이터를 가져옴
+    not_sorted = cache.get('joonggo_list')
+    # 캐시에 저장된 데이터가 만료된 상태면 db에서 다시 데이터를 가져옴
+    if not_sorted is None:
+        print(request.GET)
+
+        cat = request.GET.get('category', 'all')
+        keyword = request.GET.get('keyword', '*')
+        id_list = list()
+        category = ''
+        if cat == "001":
+            category = '데스크탑/본체'
+        elif cat == "002":
+            category = '모니터'
+        elif cat == "003":
+            category = 'CPU/메인보드'
+        elif cat == "004":
+            category = '메모리/VGA'
+        elif cat == "005":
+            category = 'HDD/SDD/ODD'
+        elif cat == "006":
+            category = "케이스/파워/쿨러"
+        elif cat == "007":
+            category = "프린터/복합기/잉크/토너"
+        elif cat == "008":
+            category = '소모품'
+        else:
+            category = 'all'
+        print(f'\n{category}\n{keyword}\n')
+        # db에서 중고거래 데이터를 가져온다
+        try:
+            db = connection.cursor()
+            qry = ''
+            if category == 'all':
+                if keyword == '*':
+                    qry = f"SELECT url, platform, issoldout, title, price, text, isad FROM dream_joonggo.joonggo_data ORDER BY id LIMIT 1000;"
+                else:
+                    id_list = Searcher.Search(connection,keyword) 
+                    print(id_list[0].url)
+                    qry = f"""SELECT url, platform, issoldout, title, price, text, isad, id FROM dream_joonggo.joonggo_data WHERE url IN ({','.join("'" + str(i.url) + "'" for i in id_list)}) ORDER BY id LIMIT 1000;"""
+            else:
+                if keyword == '*':
+                    qry = f"SELECT url, platform, issoldout, title, price, text, isad FROM dream_joonggo.joonggo_data WHERE maincategory = '{category}' OR subcategory = '{category}' ORDER BY id LIMIT 1000;"
+                else:
+                    id_list = Searcher.Search(connection,keyword) 
+                    print(id_list[0].url)
+                    qry = f"""SELECT url, platform, issoldout, title, price, text, isad, id FROM dream_joonggo.joonggo_data WHERE url IN ({','.join("'" + str(i.url) + "'" for i in id_list)}) AND (maincategory = '{category}' OR subcategory = '{category}') ORDER BY id LIMIT 1000;"""
             db.execute(qry)
             data = db.fetchall()
             # print(data)
@@ -158,22 +227,39 @@ def search(request):
 
             # print(imgdata_dict)
 
-            for res in data:
-                url = res[0]
-                # print(imgdata_dict.get(url, []))
-                row = {'url': url,
-                    'platform': res[1],
-                    'issoldout': res[2],
-                    'title': res[3],
-                    'price': res[4],
-                    'text': res[5],
-                    'isad': res[6],
-                    'imgurl': imgdata_dict.get(url, [])}
-                joonggo.append(row)
+            for dict2 in data:
+                    for dict1 in id_list:
+                        if dict1.url == dict2[0]:
+                            merged_dict = {
+                                'url': dict2[0],
+                                'platform': dict2[1],
+                                'title' : dict2[3],
+                                'price' : dict2[4],
+                                'text' : dict2[5],
+                                'isad' : dict2[6],
+                                'issoldout' : dict2[2],
+                                'score' : dict1.score,
+                                'id' : dict2[7],
+                                'imgurl': imgdata_dict.get(dict2[0],[])
+                            }
+                            joonggo.append(merged_dict)
         except Exception as err:
             connection.rollback()
             print("에러 발생")
             print(err)
+        
+        # print(joonggo[0])
+        # 데이터를 캐시에 저장
+        cache.set('joonggo_list', joonggo, timeout=3600)  # 1시간 동안 유효
+        return render(request, 'search.html', {'joonggo_list': joonggo})
+    # 이외에는 캐시 데이터를 가져와서 정렬만 한다.
+    else:
+        sort_option = request.GET.get('sort', 'id')
+        sort_option2 = request.GET.get('sort2', 'ASCENDING')
+        if sort_option2 == 'ASCENDING':
+            joonggo = sorted(not_sorted, key=lambda x: x[f'{sort_option}'])
+        elif sort_option2 == 'DESCENDING':
+            joonggo = sorted(not_sorted, key=lambda x: x[f'{sort_option}'], reverse=True)
         
         # print(joonggo[0])
         return render(request, 'search.html', {'joonggo_list': joonggo})
