c52f6cfe886ce695faf3255417ede30e0ed59171
twkim991 <darkbell991@gmail.com>
2023-11-17 00:51:24 +0900

    데이터베이스 배포

--- db_settings.py
+++ db_settings.py
@@ -1,13 +1,15 @@
 # DATABASE 연결
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'dream_joonggo', # DB명
-        'USER': 'root', # 데이터베이스 계정
-        'PASSWORD': 'darkwing991', # 계정 비밀번호
-        'HOST': 'localhost', # 데이테베이스 주소(IP)
-        'PORT': '3306', # 데이터베이스 포트(보통은 3306)
-    }
+  'default': {
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': 'dream_joonggo',
+    'HOST': 'aws.connect.psdb.cloud',
+    'PORT': '3306',
+    'USER': 'b3m09mx2prgyo1smqr5n',
+    'PASSWORD': 'pscale_pw_E7GlHv7exqCP6GcOavy5YXemfLllsn0i41w1eP4v7qn',
+    'OPTIONS': {'ssl': {'ca': 'C:\WORK\dream_webpage\cacert.pem'}, 
+                'charset': 'utf8mb4'}
+  }
 }
 
 SECRET_KEY = "django-insecure-2uz#p9%w^fq(4@_+x#hccw@$humlgi=wx8m6dfj1@x0!7asx_&";
